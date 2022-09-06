5cb2592e11feac0e819421ef91b5f4c05b2a3ce6
limtete <come.across.time@gmail.com>
2022-09-06 12:02:40 +0800

    Lab09 Done.

--- labs/lab09/tail-replicate.scm
+++ labs/lab09/tail-replicate.scm
@@ -1,5 +1,9 @@
 (define (tail-replicate x n)
-  ; BEGIN
-  'replace-this-line
-  ; END
+    ; BEGIN
+    (define (add-to-first lst x n)
+    ; Helper function, store list in lst and update each turn
+        (if (= n 0) lst
+            (add-to-first (cons x lst) x (- n 1))))
+    (add-to-first nil x n)
+    ; END
 )
