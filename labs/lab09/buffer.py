8107256e47620d97ff6f31082e307fe3b7c2ee3c
limtete <come.across.time@gmail.com>
2022-09-06 09:52:24 +0800

    Lab9, Q1, Done.

--- labs/lab09/buffer.py
+++ labs/lab09/buffer.py
@@ -54,19 +54,26 @@ class Buffer:
         exhausted its source, returns None."""
         # BEGIN PROBLEM 1
         "*** YOUR CODE HERE ***"
+        if not self.more_on_line():
+            return None
+        else:
+            return self.current_line.pop(self.index)
         # END PROBLEM 1
 
     def current(self):
         """Return the current element, or None if none exists."""
-        while _________:
+        while True:
             try:
                 # BEGIN PROBLEM 1
                 "*** YOUR CODE HERE ***"
+                if not self.more_on_line():
+                    self.current_line = next(self.source)
+                break
                 # END PROBLEM 1
             except StopIteration:
                 self.current_line = ()
                 return None
-        return __________
+        return self.current_line[self.index]
 
     def more_on_line(self):
         return self.index < len(self.current_line)
