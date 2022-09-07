7f7ff0caf4b775d7e726dc5c38383f849440ea23
limtete <come.across.time@gmail.com>
2022-09-07 21:23:58 +0800

    Proj4, P15, Done.

--- proj/scheme/questions.scm
+++ proj/scheme/questions.scm
@@ -6,10 +6,28 @@
 ;; Problem 15
 ;; Returns a list of two-element lists
 (define (enumerate s)
-  ; BEGIN PROBLEM 15
-  'replace-this-line
-  )
-  ; END PROBLEM 15
+; BEGIN PROBLEM 15
+    ; ================= Version 1: O(n^2) =================
+    ; Tail Recursion by append, O(n^2)
+    ; (define (enum-tail s n lst)
+    ;     (if (null? s) lst
+    ;         (enum-tail (cdr s) (+ n 1)
+    ;             (append lst (cons (cons n (cons (car s) nil)) nil)))))
+    ; (if (null? s) nil
+    ;     (enum-tail (cdr s) 1 (cons (cons 0 (cons (car s) nil)) nil))))
+
+    ; ================= Version 2: O(n) =================
+    ; Tail Recursion then reverse
+    (define (reverse s)
+        (define (reverse-tail s res)
+            (if (null? s) res
+                (reverse-tail (cdr s) (cons (car s) res))))
+            (reverse-tail s nil))
+    (define (rev-enum s n lst)
+        (if (null? s) lst
+            (rev-enum (cdr s) (+ n 1) (cons (cons n (cons (car s) nil)) lst))))
+    (reverse (rev-enum s 0 nil)))
+; END PROBLEM 15
 
 ;; Problem 16
 
