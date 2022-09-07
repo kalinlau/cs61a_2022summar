649fbdb7088b0c0e9ce4f352adf754c7b0bfd487
limtete <come.across.time@gmail.com>
2022-09-07 10:22:47 +0800

    Proj4, P4, Done.

--- proj/scheme/scheme_forms.py
+++ proj/scheme/scheme_forms.py
@@ -38,6 +38,15 @@ def do_define_form(expressions, env):
         validate_form(expressions, 2, 2)  # Checks that expressions is a list of length exactly 2
         # BEGIN PROBLEM 4
         "*** YOUR CODE HERE ***"
+        print('DEBUG:', expressions)
+        print('DEBUG:', signature)
+        expr = expressions.rest
+        print('DEBUG:', expr.first)
+        if scheme_atomp(expr.first):
+            env.define(signature, expr.first)
+        else:
+            env.define(signature, scheme_eval(expr.first, env))
+        return signature
         # END PROBLEM 4
     elif isinstance(signature, Pair) and scheme_symbolp(signature.first):
         # defining a named procedure e.g. (define (f x y) (+ x y))
