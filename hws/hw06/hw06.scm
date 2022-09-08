ec28fa29c5a3aaf3070936a3d4bb569598fa3fb2
limtete <come.across.time@gmail.com>
2022-09-08 15:16:51 +0800

    HW6, Done.

--- hws/hw06/hw06.scm
+++ hws/hw06/hw06.scm
@@ -1,11 +1,27 @@
 (define (cddr s) (cdr (cdr s)))
 
-(define (cadr s) 'YOUR-CODE-HERE)
+(define (cadr s)
+    (car (cdr s)))
 
-(define (caddr s) 'YOUR-CODE-HERE)
+(define (caddr s)
+    (car (cddr s)))
 
-(define (interleave lst1 lst2) 'YOUR-CODE-HERE)
+; TODO: Tail Recursion
+;; adding auxillary lst to memorize.
+;; (define (interleave-aux lst1 lst2 mem))
+;; do the assembling in mem then call interleave-aux as the last step.
+(define (interleave lst1 lst2)
+    (if (null? lst1) lst2
+        (if (null? lst2) lst1
+            (cons (car lst1) (interleave lst2 (cdr lst1))))))
 
-(define (my-filter pred lst) 'YOUR-CODE-HERE)
+(define (my-filter pred lst)
+    (if (null? lst) nil
+        (if (pred (car lst)) (cons (car lst) (my-filter pred (cdr lst)))
+            (my-filter pred (cdr lst)))))
 
-(define (concatenate s) 'YOUR-CODE-HERE)
+(define (concatenate s)
+    (define (concat-aux s mem)
+        (if (null? s) mem
+            (concat-aux (cdr s) (append mem (car s)))))
+    (concat-aux s nil))
